bbb302cdba9e2c33a4bb22a557d6b79ddc3a7161
blue <playbynumbers@gmail.com>
2020-05-11 22:40:57 +0100

    Simplify PSPaperCut project file

--- PSPaperCut/PSPaperCut.psm1
+++ PSPaperCut/PSPaperCut.psm1
@@ -1,6 +1,2 @@
-if (Test-Path (Join-Path $PSScriptRoot Private)) {
-    Join-Path (Join-Path $PSScriptRoot Private) *.ps1 -Resolve  | ForEach-Object {. $_}
-}
-if (Test-Path (Join-Path $PSScriptRoot Public)) {
-    Join-Path (Join-Path $PSScriptRoot Public) *.ps1 -Resolve  | ForEach-Object {. $_}
-}
+Join-Path (Join-Path $PSScriptRoot Private) *.ps1 -Resolve  | ForEach-Object {. $_}
+Join-Path (Join-Path $PSScriptRoot Public) *.ps1 -Resolve  | ForEach-Object {. $_}
